498813e000e12a7a091d3d46a466a5eb7c6d0aaf
Bob Sanlon <bob@aleph-labs.com>
2023-08-10 16:18:27 +0800

    env

--- app/(root)/(routes)/game/page.tsx
+++ app/(root)/(routes)/game/page.tsx
@@ -1,7 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import NewGameForm from "./components/new-game-form";
 
-export default async function NewGamePage() {
+export default async function NewGamePage({ gameId }) {
 
     const players = await prismadb.player.findMany();
     const formattedPlayers = players.map((item) => ({
