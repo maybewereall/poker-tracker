59935ec5291e42f80ec051e6abced28be3ec2cf4
Bob Sanlon <bob@aleph-labs.com>
2023-08-29 16:03:32 +0800

    fix date error

--- components/forms/create-game.tsx
+++ components/forms/create-game.tsx
@@ -72,47 +72,55 @@ const NewGameForm: React.FC<INewGameFormProps> = ({
         },
     });
 
-    
+
 
     return (
-        
+
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
                 <FormField
-                    name="date"
                     control={form.control}
+                    name="date"
                     render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="block h-[17px]">Date</FormLabel>
-                            <FormControl>
-                                <Popover>
-                                    <PopoverTrigger asChild>
+                        <FormItem className="flex flex-col mb-2">
+                            <FormLabel>Date</FormLabel>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <FormControl>
                                         <Button
                                             variant={"outline"}
                                             className={cn(
-                                                "w-full justify-start text-left font-normal",
-                                                !date && "text-muted-foreground"
+                                                "w-full pl-3 text-left font-normal",
+                                                !field.value && "text-muted-foreground"
                                             )}
                                             disabled={loading}
                                         >
-                                            <CalendarIcon className="mr-2 h-4 w-4" />
-                                            {date ? format(date, "do LLL yy") : <span>Pick a date</span>}
+                                            {field.value ? (
+                                                format(field.value, "do LLL yyyy")
+                                            ) : (
+                                                <span>Pick a date</span>
+                                            )}
+                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                         </Button>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-auto p-0">
-                                        <Calendar
-                                            mode="single"
-                                            selected={date}
-                                            onSelect={setDate}
-                                            initialFocus
-                                            {...field}
-                                        />
-                                    </PopoverContent>
-                                </Popover>
-                            </FormControl>
+                                    </FormControl>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                        mode="single"
+                                        selected={field.value}
+                                        onSelect={field.onChange}
+                                        disabled={(date) =>
+                                            date > new Date() || date < new Date("1900-01-01")
+                                        }
+                                        initialFocus
+                                    />
+                                </PopoverContent>
+                            </Popover>
+                            <FormMessage />
                         </FormItem>
                     )}
                 />
+
                 <FormField
                     name="initial_buyin"
                     control={form.control}
